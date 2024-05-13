ecd49a3c2218d05f89cd425e042b5886bf1be9f8
jaypbazar <jabazar@my.cspc.edu.ph>
2024-05-14 00:14:55 +0800

    3rd commit

--- main.py
+++ main.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route("/records/")
+def records():
+    return render_template('records.html')
+
+@app.route("/students")
+def students():
+    return render_template('students.html')
+
+@app.route("/instructors")
+def instructors():
+    return render_template('instructors.html')
+
+@app.route("/grades")
+def grades():
+    return render_template('grades.html')
+
 if __name__ == '__main__':
     app.run()
